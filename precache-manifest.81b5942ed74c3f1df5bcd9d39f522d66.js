e0f4f44002dcab92df8cb415143310a2020ef837
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-11-02 14:27:17 +0100

    Updates

---
+++ precache-manifest.81b5942ed74c3f1df5bcd9d39f522d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8746b954c1782e79458ab31148448a",
+    "url": "/react-pwa-test/index.html"
+  },
+  {
+    "revision": "7a7a53ba41753cf0b1a3",
+    "url": "/react-pwa-test/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d437f79d019b37e9410b",
+    "url": "/react-pwa-test/static/css/main.211f08f0.chunk.css"
+  },
+  {
+    "revision": "7a7a53ba41753cf0b1a3",
+    "url": "/react-pwa-test/static/js/2.c2f0d1c3.chunk.js"
+  },
+  {
+    "revision": "d437f79d019b37e9410b",
+    "url": "/react-pwa-test/static/js/main.1193c51d.chunk.js"
+  },
+  {
+    "revision": "f8f7fe5ca163b41a3d01",
+    "url": "/react-pwa-test/static/js/runtime-main.255739ae.js"
+  },
+  {
+    "revision": "71f357d4c4480ca633b31c2ffc29607d",
+    "url": "/react-pwa-test/static/media/bg.71f357d4.jpg"
+  }
+]);
